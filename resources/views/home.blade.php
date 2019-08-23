12941b8863e017e765208b5b6624631e2259eb63
Norwin Guerreo <norwingcruz@gmail.com>
2019-08-22 19:33:23 -0500

    home update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,5 +15,38 @@
 		<div class="container">
 			<div class="text-center mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum omnis, molestias quo natus consectetur <a href="#">sint quibusdam rem? Deserunt</a></div>
 		</div>
+
+		<section class="card container mt-5 markets section">
+			<h2 class="titul"><b>Markets</b></h2>
+		</section>
+
+		<section class="container mt-3 why-us section">
+			<h2 class="titul c-primary mb-5">Why Our Clients Choose Us</h2>
+			<div class="row">
+				<div class="col-sm-12 col-md-4 mb-4 values">
+					<img src="/images/responsability.svg" alt="responsability" class="img-fluid">
+					<h4 class="text-center mt-2"><b>Responsability</b></h4>
+				</div>
+				<div class="col-sm-12 col-md-4 mb-4 values">
+					<img src="/images/commitment.svg" alt="commitment" class="img-fluid">
+					<h4 class="text-center mt-2"><b>Commitment</b></h4>
+				</div>
+				<div class="col-sm-12 col-md-4 mb-4 values">
+					<img src="/images/trust.svg" alt="trust" class="img-fluid">
+					<h4 class="text-center mt-2"><b>Trust</b></h4>
+				</div>
+			</div>
+		</section>
+
+		<section class="container mt-3 our-coin section">
+			<h2 class="titul c-secundary">Bitfusion Coin</h2>
+			<div class="row">
+				<div class="col-sm-12 col-md-6">
+					<p><b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptatem facilis beatae atque, distinctio corporis natus unde.</b></p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum cupiditate officiis temporibus quisquam reiciendis</p>
+					<a href="#" class="btn">See more</a>
+				</div>
+			</div>
+		</section>
 	</div>
 @endsection
---
+++ resources/views/template/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+	<img src="/images/logo-full-color.svg" alt="logo">
+	<div class="information">
+		<p class="text-right">
+			<b class="c-secundary">Contact</b> <b>Tel</b> +57 000 000 <b>Mail:</b> info@bitfusion.com <br>
+			Copyright &copy; {{ date('Y') }} All right reserved
+		</p>
+	</div>
+</footer>
